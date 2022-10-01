715096ed19b9786bbba19f4cb326c4151c9b9331
liskfly <1398365527@qq.com>
2022-10-01 18:07:53 +0800

    测试homeView分支上传

---
+++ src/views/HomeView/HomeHead.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Headlines">
+    <div>
+      <span>YiMagazine</span>
+      <img src="./Yg.png">
+    </div>
+    <div></div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
+    <HomeHead></HomeHead>
   </div>
 </template>
 
 <script>
-
+import HomeHead from './HomeHead'
 
 export default {
   name: 'HomeView',
   components: {
- 
+    HomeHead
   }
 }
 </script>
